4378bd85a8cea8c8666279e754dabfb43ae818d0
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-22 11:40:47 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import mongoose from "mongoose";
 require("dotenv").config();
 mongoose.set("debug", true);
 mongoose
-  .connect("mongodb+srv://username:password@dani01backend.pcjwsdb.mongodb.net/ApiBackend?retryWrites=true&w=majority&appName=AtlasApp")
+  .connect(
+    "mongodb+srv://username:password@dani01backend.pcjwsdb.mongodb.net/ApiBackend?retryWrites=true&w=majority&appName=AtlasApp"
+  )
   .then((_) => {
     console.log("Connected to db");
-    app.listen(3000, () => {
+    app.listen(8080, () => {
       console.log("Server listening on port 3000");
     });
   })
