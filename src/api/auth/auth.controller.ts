5a581f4c5063e476ba16865d9060beee04534e7f
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-22 12:47:34 +0200

    Update auth.controller.ts

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import { omit, pick } from "lodash";
 import { BankAccountExistsError } from "../../errors/bank-account-exist";
 import passport from "passport";
 import * as jwt from "jsonwebtoken";
-import  BankAccountService  from "../bank-account/bank-account.service";
+import BankAccountService from "../bank-account/bank-account.service";
 
 const JWT_SECRET = "secret";
 
@@ -35,7 +35,6 @@ export const login = async (
   next: NextFunction
 ) => {
   passport.authenticate("local", (err, user, info) => {
-    //console.log("paaport auth");
     if (err) {
       return next(err);
     }
