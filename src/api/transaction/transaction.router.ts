bb7c81a1a8965997aaf27f6e3fd4503084f8d819
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-22 09:22:17 +0200

    Update transaction.router.ts

--- src/api/transaction/transaction.router.ts
+++ src/api/transaction/transaction.router.ts
@@ -1,3 +1,12 @@
 import { Router } from "express";
+import { validate } from "../../utils/validation.middleware";
+import { TransictionDTO } from "./transaction.dto";
+import { transaction } from "./transaction.controller";
+import { isAuthenticated } from "../../utils/auth/authenticated.middleware";
 
 const router = Router();
+
+router.use(isAuthenticated);
+router.post("/", validate(TransictionDTO, "body"), transaction);
+
+export default router;
