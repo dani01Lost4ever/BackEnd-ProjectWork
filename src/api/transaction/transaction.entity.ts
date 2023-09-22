c451a41cbef18bc536d8e22fc2f9bafa2793943e
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-22 09:22:17 +0200

    Update transaction.entity.ts

--- src/api/transaction/transaction.entity.ts
+++ src/api/transaction/transaction.entity.ts
@@ -1,9 +1,11 @@
-export interface transaction{
-    id: string;
-    bankaccountid: string;
-    date: Date;
-    amount: number;
-    balance: number;
-    categoryid: string;
-    description: string;
+import { Types } from "mongoose";
+
+export interface transaction {
+  id?: string;
+  bankaccountid?: Types.ObjectId;
+  date?: Date;
+  amount: number;
+  balance?: number;
+  categoryid: Types.ObjectId;
+  description: string;
 }
