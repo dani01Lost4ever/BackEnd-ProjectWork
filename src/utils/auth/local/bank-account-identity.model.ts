5e1e251bc7800781cedb4538d3428f877b87e034
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-22 09:22:16 +0200

    Add bank-account-identity.model.ts

---
+++ src/utils/auth/local/bank-account-identity.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, model } from "mongoose";
+import { BankAccountIdentity as iBankAccount } from "./bank-accout-identity.entity";
+
+export const bankAccountSchema = new mongoose.Schema<iBankAccount>({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  provider: { type: String, default: "local" },
+  credentials: {
+    type: {
+      username: String,
+      hashedPassword: String,
+    },
+  },
+});
+
+bankAccountSchema.pre("findOne", function (next) {
+  this.populate("user");
+  next();
+});
+
+export const BankAccountIdentity = model<iBankAccount>(
+  "BankAccountIdentity",
+  bankAccountSchema
+);
