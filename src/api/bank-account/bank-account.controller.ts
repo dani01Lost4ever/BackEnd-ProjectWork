991b95a8bfaee1473601475779833b3468cc7539
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-24 22:37:59 +0200

    Update bank-account.controller.ts

--- src/api/bank-account/bank-account.controller.ts
+++ src/api/bank-account/bank-account.controller.ts
@@ -1,11 +1,21 @@
 import { NextFunction, Response, Request } from "express";
 import BankAccountService from "./bank-account.service";
+import { BankAccount } from "./bank-account.model";
+import { AdminPrivilegesRequired } from "../../errors/user-errors";
 
 export const me = async (req: Request, res: Response, next: NextFunction) => {
   res.json(req.user!);
 };
 
 export const list = async (req: Request, res: Response, next: NextFunction) => {
-  const list = await BankAccountService.list();
-  res.json(list);
+  try {
+    const user = new BankAccount(req.user!);
+    if ((user.id! = "650e183d3fc6f77b6d45b657")) {
+      throw new AdminPrivilegesRequired();
+    }
+    const list = await BankAccountService.list();
+    res.json(list);
+  } catch (err: any) {
+    next(err);
+  }
 };
