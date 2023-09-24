6de1f5620b0c59475fc44ff11a6a8683ea3df7f2
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-24 22:38:01 +0200

    Update transaction-type.router.ts

--- src/api/transaction-type/transaction-type.router.ts
+++ src/api/transaction-type/transaction-type.router.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import { validate } from "../../utils/validation.middleware";
-import { listTransactionsType, getTransactionById, addTransactiontype } from './transaction-type.controller';
+import {
+  listTransactionsType,
+  getTransactionById,
+  addTransactiontype,
+} from "./transaction-type.controller";
 import { QueryTransactionTypeDTO } from "./transaction-type.dto";
+import { isAuthenticated } from "../../utils/auth/authenticated.middleware";
 
 const router = Router();
 
-router.post('/', validate(QueryTransactionTypeDTO, 'body'),addTransactiontype);
-router.get('/', validate(QueryTransactionTypeDTO, 'query'), listTransactionsType);
-router.get('/:id', getTransactionById);
+router.post(
+  "/",
+  isAuthenticated,
+  validate(QueryTransactionTypeDTO, "body"),
+  addTransactiontype
+);
+router.get("/", listTransactionsType);
+router.get("/:id", getTransactionById);
 
 export default router;
