db7578cd82159b7a0593365bffee38724b792165
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-09-27 16:41:14 +0200

    Update bank-account-exist.ts

--- src/errors/bank-account-exist.ts
+++ src/errors/bank-account-exist.ts
@@ -1,7 +1,36 @@
+import { NextFunction, Response, Request } from "express";
 export class BankAccountExistsError extends Error {
   constructor() {
     super();
     this.name = "BankAccountExists";
-    this.message = "bank account already in use";
+    this.message = "Bank account already in use";
   }
 }
+
+export class PasswordValidationError extends Error {
+  constructor() {
+    super();
+    this.name = "PasswordValidationError";
+    this.message = "Password and confirmation password are different";
+  }
+}
+
+export const bankAccountHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (
+    err instanceof BankAccountExistsError ||
+    err instanceof PasswordValidationError
+  ) {
+    res.status(401);
+    res.json({
+      error: err.name,
+      message: err.message,
+    });
+  } else {
+    next(err);
+  }
+};
